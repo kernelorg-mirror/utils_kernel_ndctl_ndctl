f1d8c5f82e1c6c0d3e2b2b0be86ee05884d5647b
Alison Schofield <alison.schofield@intel.com>
2026-04-03 18:34:41 -0700

    test/cxl-destroy-region.sh: test out-of-order destroy-region handling
    A recent change to 'cxl destroy-region' implementation ensures that
    an out of order destroy failure leaves the region intact.
    
    Test an out-of-order destroy attempt followed by in-order destroy
    success.
    
    Link: https://lore.kernel.org/r/5d924ca777e265a2faaf21d67a1649c3c0d44277.1775265383.git.alison.schofield@intel.com
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>

--- test/cxl-destroy-region.sh
+++ test/cxl-destroy-region.sh
@@ -32,6 +32,29 @@ destroy_regions()
 	fi
 }
 
+create_region()
+{
+	local decoder="$1"
+	local memdev="$2"
+	local size="$3"
+	local region
+
+	if [[ -n "$size" ]]; then
+		region=$("$CXL" create-region -d "$decoder" -m "$memdev" -s "$size" |
+			 jq -r ".region")
+	else
+		region=$("$CXL" create-region -d "$decoder" -m "$memdev" |
+			 jq -r ".region")
+	fi
+
+	if [[ -z "$region" || "$region" == "null" ]]; then
+		echo "create-region failed for decoder=$decoder memdev=$memdev"
+		err "$LINENO"
+	fi
+
+	echo "$region"
+}
+
 check_destroy_ram()
 {
 	mem=$1
@@ -67,6 +90,51 @@ check_destroy_devdax()
 	"$CXL" destroy-region "$region"
 }
 
+find_pmem_decoder()
+{
+	local mem="$1"
+	local slice="$2"
+	local decoder
+
+	decoder=$($CXL list -b cxl_test -D -d root -m "$mem" |
+		jq -r ".[] |
+		select(.pmem_capable == true) |
+		select(.nr_targets == 1) |
+		select(.max_available_extent >= $(( slice * 2 ))) |
+		.decoder" | head -n1)
+
+	[[ -z $decoder || $decoder == "null" ]] && return 1
+	echo "$decoder"
+}
+
+check_destroy_subregion_order()
+{
+	local mem="$1"
+	local slice=$((256 << 20))
+	local decoder
+	local region0=""
+	local region1=""
+
+	decoder=$(find_pmem_decoder "$mem" "$slice") || return 1
+
+	region0=$(create_region "$decoder" "$mem" "$slice")
+	region1=$(create_region "$decoder" "$mem" "$slice")
+
+	# wrong destroy order should fail
+	destroy_regions "$region0" && err "$LINENO"
+
+	# region0 should still be enabled
+	"$CXL" list -r "$region0" | jq -e 'length > 0' > /dev/null || err "$LINENO"
+
+	# regions should tear down cleanly in correct order
+	destroy_regions "$region1" || err "$LINENO"
+	"$CXL" list -r "$region1" | jq -e 'length == 0' > /dev/null || err "$LINENO"
+	destroy_regions "$region0" || err "$LINENO"
+	"$CXL" list -r "$region0" | jq -e 'length == 0' > /dev/null || err "$LINENO"
+
+	return 0
+}
+
 # Get clean slate, including auto region resources
 destroy_regions
 assert_no_regions
@@ -93,6 +161,17 @@ for mem in "${mems[@]}"; do
 	check_destroy_devdax "$mem" "$decoder"
 	found=1
 	break
+done
+[[ $found -eq 1 ]] || err "$LINENO"
+
+# test wrong-order destroy on back-to-back pmem regions
+destroy_regions
+found=0
+for mem in "${mems[@]}"; do
+	if check_destroy_subregion_order "$mem"; then
+		found=1
+		break
+	fi
 done
 [[ $found -eq 1 ]] || err "$LINENO"
 
